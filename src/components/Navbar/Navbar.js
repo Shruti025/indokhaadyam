f1af6c815bc45b63d067c042389816f4ac6fd783
Shruti025 <shrutigoyal25042001@gmail.com>
2022-04-09 16:37:37 +0530

    UI changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,14 +4,14 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className='List-container'>
-                <div className='List'>
-                    <div className='ListItem'><a className='Link' href = "/">Home</a></div>
-                    <div className='ListItem'><a className='Link' href = "/">Categories</a></div>
-                    <div className='ListItem'><a className='Link' href = "Contact">Contact</a></div>
-                    <div className='ListItem'><a className='Link' href = "/">Blog</a></div>
-                    <div className='ListItem'><a className='Link' href = "Team">Team</a></div>
-                    <div className='ListItem'><a className='Link' href = "/recipedata">Recipe-data</a></div>
-                </div>
+            <div className='List'>
+                <div className='ListItem'><a className='Link' href="/">Home</a></div>
+                <div className='ListItem'><a className='Link' href="/">Categories</a></div>
+                <div className='ListItem'><a className='Link' href="Contact">Contact</a></div>
+                <div className='ListItem'><a className='Link' href="/">Blog</a></div>
+                <div className='ListItem'><a className='Link' href="Team">Team</a></div>
+                <div className='ListItem'><a className='Link' href="/recipeform">Recipe-Form</a></div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/mobilenavbar.js
+++ src/components/Navbar/mobilenavbar.js
@@ -4,20 +4,20 @@ import './mobilenavbar.css'
 
 const MobileNavbar = () => {
 
-    const[isOpen, setOpen] = useState(false);
+    const [isOpen, setOpen] = useState(false);
 
     return (
         <div className='list-container'>
-                {isOpen && <div className='list'>
-                    <div className='listItem'><a className='link' href = "/">Home</a></div>
-                    <div className='listItem'><a className='link' href = "/">Categories</a></div>
-                    <div className='listItem'><a className='link' href = "Contact">Contact</a></div>
-                    <div className='listItem'><a className='link' href = "/">Blog</a></div>
-                    <div className='listItem'><a className='link' href = "Team">Team</a></div>
-                    <div className='ListItem'><a className='Link' href = "/recipedata">Recipe-data</a></div>
-                </div>}
-                
-                <MenuToggle isOpen = {isOpen} toggle = {() => setOpen(!isOpen)}/>
+            {isOpen && <div className='list'>
+                <div className='listItem'><a className='link' href="/">Home</a></div>
+                <div className='listItem'><a className='link' href="/">Categories</a></div>
+                <div className='listItem'><a className='link' href="Contact">Contact</a></div>
+                <div className='listItem'><a className='link' href="/">Blog</a></div>
+                <div className='listItem'><a className='link' href="Team">Team</a></div>
+                <div className='ListItem'><a className='Link' href="/recipeform">Recipe-Form</a></div>
+            </div>}
+
+            <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
         </div>
     )
 }
